faaf32cf18b70e17887af12e2de70b56a038fb68
Christian Payne <christianpayne522@gmail.com>
2022-03-01 18:27:41 -0800

    I did the thing. Now to do others.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,59 @@
 <script lang="ts">
+import Item from "./Item.svelte";
+
+let items = [
+  {
+    name:"Kale",
+    quantity: 2,
+    need: true
+  },
+  {
+    name:"Broccoli",
+    quantity: 1,
+    need: true
+  },
+  {
+    name:"Bread",
+    quantity: 1,
+    need: true
+  },
+  {
+    name:"Corn",
+    need: true
+  }
+]
+
+function OnSwipe (item) {
+  // items = items.filter(i => i != item)
+
+  items = items.map(i => {
+    if(item === i){
+      item.need = false
+    }
+    return i
+  })
+}
+
 </script>
 
-<main>
-  <h1 class="text-red-600">HEADER</h1>
+<main class="flex flex-col justify-between fixed inset-0">
+  <!-- Header -->
+  <div class="flex justify-center p-4 bg-green-900 text-white font-serif">
+    <h1 class="text-4xl">Leafy Kale</h1>
+  </div>
+  <!-- Items -->
+  <div class="grow">
+    {#each items as item}
+      <Item item={item} onSwipe={OnSwipe}/>
+    {/each}
+  </div>
+  <!-- Footer -->
+  <div class="flex-none justify-center p-2 bg-green-900 text-white font-serif text-xl">
+    <div class="flex justify-around">
+      <button class="p-4 grow">All Items</button>
+      <button class="p-4 grow">Grocery List</button>
+    </div>
+  </div>
 </main>
 
 <style>
---
+++ src/Item.svelte
@@ -0,0 +1,41 @@
+{#if item.need}
+  <Swiper
+  class="h-32 text-center text-2xl font-serif items-center m-2"
+  spaceBetween={50}
+  slidesPerView={1}
+  on:transitionEnd={e => onTransitionEnd()}
+  on:reachEnd={e => onEnd()}
+  >
+    <SwiperSlide class="flex bg-green-900 rounded-xl text-white justify-center items-center">
+      <div>
+        <p>{item.name}</p>
+        {#if item.quantity && item.quantity > 1}
+          <p>Quantity: {item.quantity}</p>
+        {/if}
+      </div>
+    </SwiperSlide>
+    <SwiperSlide></SwiperSlide>
+  </Swiper>
+{/if}
+<script>
+// Import Swiper Svelte components
+import { Swiper, SwiperSlide } from 'swiper/svelte';
+
+export let item;
+export let onSwipe;
+
+let swiped = false;
+
+function onEnd () {
+  swiped = true;
+}
+
+function onTransitionEnd () {
+  if(swiped) {
+    onSwipe(item);
+  }
+}
+
+// Import Swiper styles
+import 'swiper/css';
+</script>
