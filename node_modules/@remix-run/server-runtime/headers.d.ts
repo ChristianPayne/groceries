5190c786ba098a551e7a824142fb15b425ea006f
Christian Payne <christianpayne522@gmail.com>
2022-05-30 13:45:21 -0700

    Removed node modules

--- node_modules/@remix-run/server-runtime/headers.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ServerBuild } from "./build";
-import type { ServerRoute } from "./routes";
-import type { RouteMatch } from "./routeMatching";
-export declare function getDocumentHeaders(build: ServerBuild, matches: RouteMatch<ServerRoute>[], routeLoaderResponses: Record<string, Response>, actionResponse?: Response): Headers;
--- node_modules/@remix-run/server-runtime/server.d.ts
+++
@@ -1,5 +0,0 @@
-import type { AppLoadContext } from "./data";
-import type { ServerBuild } from "./build";
-export declare type RequestHandler = (request: Request, loadContext?: AppLoadContext) => Promise<Response>;
-export declare type CreateRequestHandlerFunction = (build: ServerBuild, mode?: string) => RequestHandler;
-export declare const createRequestHandler: CreateRequestHandlerFunction;
